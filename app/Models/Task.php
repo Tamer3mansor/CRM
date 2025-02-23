b1fe5aa39b37a0f514985ed4ae32356c5531076d
Tamer <62846154+Tamer3mansor@users.noreply.github.com>
2025-02-23 17:29:32 +0200

    " relations "

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $guarded = [];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id')->withDefault(
+            [
+                "name" => 'no name',
+            ]
+        );
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'project_id', 'id');
+    }
+    public function scopeStatus($query, $status = 'completed')
+    {
+        return $query->where('status', $status);
+    }
 }
